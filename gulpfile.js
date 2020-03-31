ee0311760a0844ace5c1be248abe851d0d6fdc77
EugeneKopyl <EugeneKopyl@gmail.com>
2020-03-31 08:58:03 +0300

    настройка gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require("gulp");
+var server = require("browser-sync").create();
+var less = require("gulp-less");
+var plumber = require("gulp-plumber");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+
+dulp.task("style", function () {
+	gulp.src("source/less/style.less")
+		.pipe(plumber())
+		.pipe(less())
+		.pipe(postcss([
+			autoprefixer()
+		]))
+		.pipe(gulp.dest("source/css"))
+});
+
+gulp.task("serve", ["style"], function() {
+	server.init({
+		server: "source/"
+	});
+	gulp.watch("source/less/**/*.less", ["style"]);
+});
